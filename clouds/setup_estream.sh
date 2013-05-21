db3f7d6bdf7e93a0925c682d373ae286b0b05b4d
br <br@taikoo.sfo>
2013-05-21 20:28:29 +0000

    Fix

--- clouds/setup_estream.sh
+++ clouds/setup_estream.sh
@@ -8,22 +8,12 @@ test -d /usr/local/estream && echo 'Already installed!' && exit 0;
 ./setup_openssl_devel.sh || exit -1
 
 SAVE=$PWD
-cd $HOME/gits
-
-if [[ -d eStream ]]
-then
-    echo "Updating eStream..."
-    cd eStream
-    git pull || exit -1
-else
-    echo "Cloning eStream..."
-    git clone macau:/git/eStream || exit -1
-    cd eStream
-fi
+cd $HOME/gits/clouds/eStream
+git pull || exit -1     # get latest version
 
 # ---
 make clean && make
-sudo sh -c "cd $HOME/gits/eStream && make install"
+sudo sh -c "cd $HOME/gits/clouds/eStream && make install"
 make clean
 sudo /sbin/chkconfig estream off        # make install puts this on by default, disable it as we'll use primers
 sudo install -m 755 $HOME/gits/eStream/bin/rc /etc/init.d/estream
