4085e5af3e09fe88325e9b36b6ab1a7d2c10ddab
br <br@taikoo.sfo>
2013-05-25 09:33:04 +0000

    --auto-comment-by-gc--

--- gen/rails/my/install.sh
+++ gen/rails/my/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-RAILS_ENV=production rake db:create
-RAILS_ENV=production rake db:migrate
+#RAILS_ENV=production rake db:create
+#RAILS_ENV=production rake db:migrate
+RAILS_ENV=bootstrap rake db:create
+RAILS_ENV=bootstrap rake db:migrate
 #RAILS_ENV=production rake db:seed -- load seed data from prime (system prime)
 
