086dc25d4d6f86e4c9366c465bf3c0cdb9707d4e
br <br@taikoo.sfo>
2013-05-26 02:35:02 +0000

    --auto-comment-by-gc--

--- clouds/setup_db.sh
+++ clouds/setup_db.sh
@@ -27,7 +27,7 @@ echo 'DELETE FROM `pins`; ALTER TABLE `pins` AUTO_INCREMENT=1;' | mysql -uroot m
 mysql -uroot my <$HOME/gits/clouds/gen/rails/my/db/pin.sql
 
 # 6. reset master so binlog isn't sucking thru all this setup
-echo "reset master;" | /usr/bin/mysql/mysql -uroot 
+echo "reset master;" | /usr/bin/mysql -uroot 
 
 # 7. stop DB
 sudo service mysqld stop
