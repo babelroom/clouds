b9b18b898a4fae4af9e9cf9dbc02e01d2514fff9
br <br@taikoo.sfo>
2013-06-02 08:19:29 +0000

    --auto-comment-by-gc--

--- clouds/clean_up.sh
+++ clouds/clean_up.sh
@@ -5,3 +5,8 @@ sudo rm -rf $HOME/src/*
 sudo rm -rf $HOME/gits/FS
 sudo rm -rf $HOME/gits/red5/.git
 
+# clear out some other stuff
+sudo rm -rf /usr/share/man      # about 20MB with gz files (uncompressible), perhaps better to do this with yum/rpm?
+sudo rm -rf /usr/share/doc
+sudo rm -rf /usr/share/selinux  # I personally take great pleasure in this one
+
