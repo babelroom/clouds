77e6211ebc0cb98afb742c20cabfe85638367d07
br <br@taikoo.sfo>
2013-06-03 05:28:21 +0000

    --auto-comment-by-gc--

--- clouds/add_system_info.sh
+++ clouds/add_system_info.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 BR=/home/br
 IK=$BR/ikeys
@@ -9,27 +9,31 @@ NETMASK=255.255.255.0
 runuser -l br -c "mkdir -p $IK"
 
 # ---
-function write_ip  {
-    DIR=$IK
-    runuser -l br -c "mkdir -p $DIR"
-    echo -n "$IP" > "/tmp/br/avi.$DEVICE"
-    runuser -l br -c "cp /tmp/br/avi.$DEVICE $DIR/$DEVICE"
+function create_dir {
+    runuser -l br -c "mkdir -p $IK"
+}
+function write_kv {
+    KEY=$1
+    VALUE=$1
+    echo -n "$VALUE" > "/tmp/br/avi.$KEY"
+    runuser -l br -c "cp /tmp/br/avi.$KEY $IK/$KEY"
     cat << EOT >> "$BR/ikeys.txt"
-$DEVICE: $IP
+$KEY: $VALUE
 EOT
 }
 
 # --- version
 BUILD=$(runuser -l br -c "cd /home/br/gits/clouds && git rev-list HEAD | wc -l")
-echo "Version...."
-`cat /home/br/gits/clouds/clouds/misc/version/stamp`$BUILD
+VERSION=$(</home/br/gits/clouds/clouds/misc/version/stamp)
+write_kv "version" $VERSION$BUILD
 
+# ---
 MAC_ADDR=$(ifconfig eth0 | sed -n 's/.*HWaddr \([a-fA-F0-9:]*\).*/\L\1/p')
 echo "   === Add Virtual Interfaces ===";
 echo "mac: [$MAC_ADDR]";
 IP=$(ifconfig eth0 | sed -n 's/.*inet addr:\([0-9.]*\)*.*/\1/p')
 DEVICE=eth0
-write_ip
+write_kv $DEVICE $IP
 echo "eth0 ip: [$IP]";
 echo "virtual IP's follow... (only expect virtual IP's on amazon ec2 instances)";
 LIP=($(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC_ADDR/local-ipv4s))
@@ -49,6 +53,6 @@ BOOTPROTO=none
 EOT
     ifup $DEVICE
     i=$[i+1]
-    write_ip
+    write_kv $DEVICE $IP
 done
 
